76343b9e864e34022b0a958ce730a15d478cc034
nishimura5 <super.nishimura@gmail.com>
2023-10-11 14:03:47 +0900

    xxx_detector.pyをクラス化

--- src/mediapipe_detector.py
+++ src/mediapipe_detector.py
@@ -4,49 +4,61 @@ import cv2
 import mediapipe as mp
 import pandas as pd
 
-video_path = "cup.mp4"
-file_name = os.path.splitext(os.path.basename(video_path))[0]
-pkl_path = f"{file_name}.pkl"
 
-cap = cv2.VideoCapture(video_path)
-mp_holistic = mp.solutions.holistic.Holistic(model_complexity=2, refine_face_landmarks=True)
+class MediaPipeDetector:
+    def __init__(self, cap):
+        self.cap = cap
+        self.model = mp.solutions.holistic.Holistic(model_complexity=2, refine_face_landmarks=True)
 
-total_frame_num = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.total_frame_num = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.number_of_keypoints = {'face': 468, 'right_hand': 21, 'left_hand': 21}
 
-number_of_keypoints = {'face': 468, 'right_hand': 21, 'left_hand': 21}
-# データの初期化
-data_dict = {"frame": [], "member": [], "keypoint": [], "x": [], "y": [], "z": [], "timestamp": []}
-for i in range(total_frame_num):
-    ret, frame = cap.read()
-    rgb_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = mp_holistic.process(rgb_img)
+    def detect(self):
+        # データの初期化
+        data_dict = {"frame": [], "member": [], "keypoint": [], "x": [], "y": [], "z": [], "timestamp": []}
+        for i in range(self.total_frame_num):
+            ret, frame = self.cap.read()
+            rgb_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = self.model.process(rgb_img)
 
-    timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)
+            timestamp = self.cap.get(cv2.CAP_PROP_POS_MSEC)
 
-    # 検出結果の取り出し
-    member_ids = ['face', 'right_hand', 'left_hand']
-    for member_id in member_ids:
-        keypoints = getattr(results, f'{member_id}_landmarks').landmark
-        if keypoints is None:
-            continue
-        for k in range(number_of_keypoints[member_id]):
-            keypoint_id = k
-            x = float(keypoints[k].x * frame_width)
-            y = float(keypoints[k].y * frame_height)
-            z = float(keypoints[k].z * frame_width)
+            # 検出結果の取り出し
+            member_ids = ['face', 'right_hand', 'left_hand']
+            for member_id in member_ids:
+                keypoints = getattr(results, f'{member_id}_landmarks').landmark
+                if keypoints is None:
+                    continue
+                for k in range(self.number_of_keypoints[member_id]):
+                    keypoint_id = k
+                    x = float(keypoints[k].x * self.frame_width)
+                    y = float(keypoints[k].y * self.frame_height)
+                    z = float(keypoints[k].z * self.frame_width)
 
-            # データの詰め込み
-            data_dict["frame"].append(i)
-            data_dict["member"].append(member_id)
-            data_dict["keypoint"].append(keypoint_id)
-            data_dict["x"].append(x)
-            data_dict["y"].append(y)
-            data_dict["z"].append(z)
-            data_dict["timestamp"].append(timestamp)
+                    # データの詰め込み
+                    data_dict["frame"].append(i)
+                    data_dict["member"].append(member_id)
+                    data_dict["keypoint"].append(keypoint_id)
+                    data_dict["x"].append(x)
+                    data_dict["y"].append(y)
+                    data_dict["z"].append(z)
+                    data_dict["timestamp"].append(timestamp)
 
-dst_df = pd.DataFrame(data_dict).set_index(["frame", "member", "keypoint"])
-print(dst_df)
-dst_df.attrs["model"] = "MediaPipe Holistic"
-dst_df.to_pickle(pkl_path)
+        self.dst_df = pd.DataFrame(data_dict).set_index(["frame", "member", "keypoint"])
+        self.dst_df.attrs["model"] = "MediaPipe Holistic"
+
+    def to_pickle(self):
+        self.dst_df.to_pickle(pkl_path)
+
+
+if __name__ == "__main__":
+    video_path = "cup.mp4"
+    file_name = os.path.splitext(os.path.basename(video_path))[0]
+    pkl_path = f"{file_name}.pkl"
+
+    cap = cv2.VideoCapture(video_path)
+    detector = MediaPipeDetector(cap)
+    detector.detect()
+    detector.to_pickle()
--- src/yolo_detector.py
+++ src/yolo_detector.py
@@ -4,45 +4,58 @@ import cv2
 from ultralytics import YOLO
 import pandas as pd
 
-video_path = "taiso.mp4"
-file_name = os.path.splitext(os.path.basename(video_path))[0]
-pkl_path = f"{file_name}.pkl"
 
-cap = cv2.VideoCapture(video_path)
-model = YOLO(model="yolov8x-pose-p6.pt")
+class YoloDetector:
+    def __init__(self, cap, show=False):
+        self.cap = cap
+        self.model = YOLO(model="yolov8x-pose-p6.pt")
 
-total_frame_num = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.total_frame_num = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.number_of_keypoints = 17
+        self.show = show
 
-number_of_keypoints = 17
-# データの初期化
-data_dict = {"frame": [], "member": [], "keypoint": [], "x": [], "y": [], "conf": [], "timestamp": []}
-for i in range(total_frame_num):
-    ret, frame = cap.read()
-    result = model.track(frame, verbose=False, persist=True, classes=0, show=True)
+    def detect(self):
+        # データの初期化
+        data_dict = {"frame": [], "member": [], "keypoint": [], "x": [], "y": [], "conf": [], "timestamp": []}
+        for i in range(self.total_frame_num):
+            ret, frame = self.cap.read()
+            result = self.model.track(frame, verbose=False, persist=True, classes=0, show=self.show)
 
-    timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)
+            timestamp = self.cap.get(cv2.CAP_PROP_POS_MSEC)
 
-    # 検出結果の取り出し
-    result_keypoints = result[0].keypoints.data
-    result_boxes = result[0].boxes.data
-    for keypoints, boxes in zip(result_keypoints, result_boxes):
-        member_id = int(boxes[4])
-        for k in range(number_of_keypoints):
-            keypoint_id = k
-            x = float(keypoints[k][0])
-            y = float(keypoints[k][1])
-            conf = float(keypoints[k][2])
+            # 検出結果の取り出し
+            result_keypoints = result[0].keypoints.data
+            result_boxes = result[0].boxes.data
+            for keypoints, boxes in zip(result_keypoints, result_boxes):
+                member_id = int(boxes[4])
+                for k in range(self.number_of_keypoints):
+                    keypoint_id = k
+                    x = float(keypoints[k][0])
+                    y = float(keypoints[k][1])
+                    conf = float(keypoints[k][2])
 
-            # データの詰め込み
-            data_dict["frame"].append(i)
-            data_dict["member"].append(member_id)
-            data_dict["keypoint"].append(keypoint_id)
-            data_dict["x"].append(x)
-            data_dict["y"].append(y)
-            data_dict["conf"].append(conf)
-            data_dict["timestamp"].append(timestamp)
+                    # データの詰め込み
+                    data_dict["frame"].append(i)
+                    data_dict["member"].append(member_id)
+                    data_dict["keypoint"].append(keypoint_id)
+                    data_dict["x"].append(x)
+                    data_dict["y"].append(y)
+                    data_dict["conf"].append(conf)
+                    data_dict["timestamp"].append(timestamp)
 
-dst_df = pd.DataFrame(data_dict).set_index(["frame", "member", "keypoint"])
-print(dst_df)
-dst_df.attrs["model"] = "YOLOv8 x-pose-p6"
-dst_df.to_pickle(pkl_path)
+        self.dst_df = pd.DataFrame(data_dict).set_index(["frame", "member", "keypoint"])
+        self.dst_df.attrs["model"] = "YOLOv8 x-pose-p6"
+
+    def to_pickle(self):
+        self.dst_df.to_pickle(pkl_path)
+
+
+if __name__ == "__main__":
+    video_path = "taiso.mp4"
+    file_name = os.path.splitext(os.path.basename(video_path))[0]
+    pkl_path = f"{file_name}.pkl"
+
+    cap = cv2.VideoCapture(video_path)
+    detector = YoloDetector(cap)
+    detector.detect()
+    detector.to_pickle()
