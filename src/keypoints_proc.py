056f5040d825d209037a2f96d3fbecf7a3e4b200
nishimura5 <super.nishimura@gmail.com>
2023-10-31 17:52:15 +0900

    テストデータによる動作確認ををmainに追加

--- src/keypoints_proc.py
+++ src/keypoints_proc.py
@@ -28,9 +28,51 @@ def thinning(src_df, thinning: int):
 
 
 if __name__ == "__main__":
-    pd.set_option("display.min_rows", 500)
-    pd.set_option("display.max_rows", 500)
+    import matplotlib.pyplot as plt
+    pd.set_option("display.min_rows", 100)
+    pd.set_option("display.max_rows", 100)
 
-    src_df = pd.read_pickle("trk/cup.pkl")
-    diff_df = calc_speed(src_df, 1)
-    print(diff_df)
+    # テストデータ生成
+    frames = range(1, 1001)
+    members = ['test1', 'test2'] 
+    keypoints = [f'{i}' for i in range(1, 11)]
+    data = {'frame': [], 'member': [], 'keypoint': [], 'x': [], 'y': [], 'timestamp': []}
+    for f in frames:
+        for m in members:
+            for k in keypoints:
+                data['frame'].append(f)
+                data['member'].append(m)
+                data['keypoint'].append(k)
+                data['x'].append(np.sin(f * 2 * np.pi / 1000) * 50 + 50)
+                data['y'].append(np.cos(f * 2 * np.pi / 1000) * 50 + 50)
+                data['timestamp'].append(f)
+    test_df = pd.DataFrame(data).set_index(['frame', 'member', 'keypoint'])
+    
+    # calc_speed
+    dt_span = 10
+    speed_df = calc_speed(test_df, dt_span)
+    test_speed_df = pd.concat([test_df, speed_df], axis=1)
+
+    # thinning
+    thinning_val = 140
+    thinned_df = thinning(test_speed_df, thinning_val)
+
+    # グラフ描画
+    fig, ax = plt.subplots(3, 1)
+    plot_df = test_speed_df.loc[pd.IndexSlice[:, 'test1', '1'], :]
+    print(plot_df)
+    # テストデータ
+    ax[0].plot(plot_df['timestamp'], plot_df['x'], label='x')
+    ax[0].plot(plot_df['timestamp'], plot_df['y'], label='y')
+    ax[0].set_ylabel('x, y')
+    ax[0].legend()
+    # speed
+    ax[1].plot(plot_df['timestamp'], plot_df[f'dt_{dt_span}'], label='speed')
+    ax[1].set_ylim(0, 5)
+    ax[1].set_ylabel(f'speed ({dt_span})')
+    # thinning
+    ax[2].plot(thinned_df['timestamp'], thinned_df['x'], label='x')
+    ax[2].plot(thinned_df['timestamp'], thinned_df['y'], label='y')
+    ax[2].set_ylabel(f'x, y ({thinning_val})')
+
+    plt.show()
