34006fcadbe1b2c27a4052cb4ebbb56eb453fac2
nishimura5 <super.nishimura@gmail.com>
2023-12-13 10:16:19 +0900

    メッセージ表示の分岐を追加

--- launcher.bat
+++ launcher.bat
@@ -1,11 +1,17 @@
 @echo off
 
 where rye.exe > nul 2>&1
-if %errorlevel% == 0 (
-    rye run launcher
-) else (
+if %errorlevel% NEQ 0 (
     echo rye.exe was not found in PATH
-    python .\src\launcher.py
+    pause
+    exit
 )
+if not exist ".\.venv\" (
+    echo .venv folder was not found
+    pause
+    exit
+)
+
+rye run launcher
 
 pause
