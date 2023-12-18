a07e80023d2ad153f6717629d30ae5cbb1ac5d3f
nishimura5 <super.nishimura@gmail.com>
2023-12-18 10:18:45 +0900

    samplecodeのファイル入力方法を変更

--- src/samplecode/filter_track.py
+++ src/samplecode/filter_track.py
@@ -5,26 +5,26 @@ import pandas as pd
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from python_senpai import keypoints_proc
+from python_senpai import file_inout
 from python_senpai import time_format
 
-track_list_path = os.path.join(os.path.dirname(__file__), "track_list.txt")
-
-with open(track_list_path, "r") as f:
-    trk_list = f.read().splitlines()
 
 DURATION_MSEC_THRESHOLD = 1000 * 60
 
-for trk_path in trk_list:
-    trk_df = pd.read_pickle(trk_path)
+# macは/Users/username
+video_dir = "%USERPROFILE%/Videos"
+trk_path = file_inout.open_pkl(video_dir)
 
-    print("member, start, end, duration")
-    groups = trk_df.groupby("member")
-    for name, group in groups:
-        duration = group["timestamp"].max() - group["timestamp"].min()
-        # 写っている時間がDURATION_MSEC_THRESHOLDより短いものは除外
-        if duration < DURATION_MSEC_THRESHOLD:
-            continue
-        time_min = time_format.msec_to_timestr_with_fff(group["timestamp"].min())
-        time_max = time_format.msec_to_timestr_with_fff(group["timestamp"].max())
-        duration_str = time_format.msec_to_timestr_with_fff(duration)
-        print(f"{name}, {time_min}, {time_max}, {duration_str}")
+trk_df = pd.read_pickle(trk_path)
+
+print("member, start, end, duration")
+groups = trk_df.groupby("member")
+for name, group in groups:
+    duration = group["timestamp"].max() - group["timestamp"].min()
+    # 写っている時間がDURATION_MSEC_THRESHOLDより短いものは除外
+    if duration < DURATION_MSEC_THRESHOLD:
+        continue
+    time_min = time_format.msec_to_timestr_with_fff(group["timestamp"].min())
+    time_max = time_format.msec_to_timestr_with_fff(group["timestamp"].max())
+    duration_str = time_format.msec_to_timestr_with_fff(duration)
+    print(f"{name}, {time_min}, {time_max}, {duration_str}")
--- src/samplecode/print_track_file.py
+++ src/samplecode/print_track_file.py
@@ -5,28 +5,27 @@ import pandas as pd
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from python_senpai import keypoints_proc
+from python_senpai import file_inout
 
 
-track_list_path = os.path.join(os.path.dirname(__file__), "track_list.txt")
+# macは/Users/username
+video_dir = "%USERPROFILE%/Videos"
+trk_path = file_inout.open_pkl(video_dir)
 
-with open(track_list_path, "r") as f:
-    trk_list = f.read().splitlines()
+trk_df = pd.read_pickle(trk_path)
+print("[Head]")
+if keypoints_proc.has_keypoint(trk_df) is True:
+    print(trk_df.head())
+else:
+    print("No keypoint index in {}".format(trk_path))
+    print(trk_df.head())
 
-for trk_path in trk_list:
-    trk_df = pd.read_pickle(trk_path)
-    print("[Head]")
-    if keypoints_proc.has_keypoint(trk_df) is True:
-        print(trk_df.head())
-    else:
-        print("No keypoint index in {}".format(trk_path))
-        print(trk_df.head())
+print("[attrs]")
+print(trk_df.attrs)
 
-    print("[attrs]")
-    print(trk_df.attrs)
-
-    # frameとmemberの個数を表示
-    print("[index]")
-    print("frame :", trk_df.index.get_level_values("frame").unique().size)
-    print("member:", trk_df.index.get_level_values("member").unique().size)
-    if keypoints_proc.has_keypoint(trk_df) is True:
-        print("keypoint:", trk_df.index.get_level_values("keypoint").unique().size)
+# frameとmemberの個数を表示
+print("[index]")
+print("frame :", trk_df.index.get_level_values("frame").unique().size)
+print("member:", trk_df.index.get_level_values("member").unique().size)
+if keypoints_proc.has_keypoint(trk_df) is True:
+    print("keypoint:", trk_df.index.get_level_values("keypoint").unique().size)
