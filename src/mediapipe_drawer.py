6086d18027b39526aa546ae67939f4bfb3b01e43
nishimura5 <super.nishimura@gmail.com>
2024-02-13 18:47:09 +0900

    faceのkeypoint更新

--- src/mediapipe_drawer.py
+++ src/mediapipe_drawer.py
@@ -42,16 +42,30 @@ class Annotate:
             self.lines(kps, True)
             kps = [self.kps[446], self.kps[339], self.kps[254], self.kps[253], self.kps[252], self.kps[256], self.kps[341], self.kps[463], self.kps[414], self.kps[286], self.kps[258], self.kps[257], self.kps[259], self.kps[260], self.kps[467]]
             self.lines(kps, True)
+
+            # left eyebrow
+            kps = [self.kps[336], self.kps[296], self.kps[334], self.kps[293], self.kps[300], self.kps[276], self.kps[283], self.kps[282], self.kps[295], self.kps[285]]
+            self.lines(kps, True)
+            # right eyebrow
+            kps = [self.kps[70], self.kps[63], self.kps[105], self.kps[66], self.kps[107], self.kps[55], self.kps[65], self.kps[52], self.kps[53], self.kps[46]]
+            self.lines(kps, True)
+
+            # mouth
             kps = [self.kps[13], self.kps[312], self.kps[311], self.kps[310], self.kps[415], self.kps[308], self.kps[324], self.kps[318], self.kps[402], self.kps[317], self.kps[14], self.kps[87], self.kps[178], self.kps[88], self.kps[95], self.kps[78], self.kps[191], self.kps[80], self.kps[81], self.kps[82]]
             self.lines(kps, True)
             kps = [self.kps[0], self.kps[267], self.kps[269], self.kps[270], self.kps[409], self.kps[291], self.kps[375], self.kps[321], self.kps[405], self.kps[314], self.kps[17], self.kps[84], self.kps[181], self.kps[91], self.kps[146], self.kps[61], self.kps[185], self.kps[40], self.kps[39], self.kps[37]]
             self.lines(kps, True)
+
+            # center
             kps = [self.kps[10], self.kps[151], self.kps[9], self.kps[8], self.kps[168], self.kps[6], self.kps[197], self.kps[195], self.kps[5], self.kps[4], self.kps[1], self.kps[19], self.kps[94], self.kps[2], self.kps[164], self.kps[0]]
             self.lines(kps, False)
             kps = [self.kps[17], self.kps[18], self.kps[200], self.kps[175], self.kps[152]]
             self.lines(kps, False)
+
+            # nose
             kps = [self.kps[98], self.kps[64], self.kps[48], self.kps[115], self.kps[220], self.kps[45], self.kps[4], self.kps[275], self.kps[440], self.kps[344], self.kps[278], self.kps[294], self.kps[327]]
             self.lines(kps, False)
+
             kps = [self.kps[35], self.kps[116], self.kps[123], self.kps[147], self.kps[213], self.kps[138], self.kps[135], self.kps[169], self.kps[211], self.kps[204], self.kps[106], self.kps[43], self.kps[57], self.kps[186], self.kps[92], self.kps[165], self.kps[203], self.kps[142], self.kps[100], self.kps[120], self.kps[231], self.kps[230], self.kps[229], self.kps[228]]
             self.lines(kps, True)
             kps = [self.kps[265], self.kps[345], self.kps[352], self.kps[376], self.kps[433], self.kps[367], self.kps[364], self.kps[394], self.kps[431], self.kps[424], self.kps[335], self.kps[273], self.kps[287], self.kps[410], self.kps[322], self.kps[391], self.kps[423], self.kps[371], self.kps[329], self.kps[349], self.kps[451], self.kps[450], self.kps[449], self.kps[448]]
