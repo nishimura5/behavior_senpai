a3dd408ce101a813ef51a31812012ec5afddd334
nishimura5 <super.nishimura@gmail.com>
2024-04-02 09:39:10 +0900

    Removed sh files for Mac

--- launcher.sh
+++
@@ -1,7 +0,0 @@
-export PYTHON_HOME="/Library/Frameworks/Python.framework/Version/3.11.6/bin"
-export PATH="$PYTHON_HOME:$PATH"
-alias python='python3'
-alias pip='pip3'
-
-. .venv/bin/activate
-python ./src/launcher.py
