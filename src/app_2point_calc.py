b0990df988d62a131ea3ec973cfe5402d3a51525
nishimura5 <super.nishimura@gmail.com>
2024-06-01 15:47:10 +0900

    Added tree list to app_2point_calc

--- src/app_2point_calc.py
+++ src/app_2point_calc.py
@@ -23,38 +23,53 @@ class App(ttk.Frame):
         width, height, dpi = temp.get_window_size()
         self.lineplot = LinePlotter(fig_size=(width / dpi, height / dpi), dpi=dpi)
 
-        cross_frame = ttk.Frame(self)
-        cross_frame.pack(pady=5)
-        self.calc_case_entry = CalcCaseEntry(cross_frame, temp.data["calc_case"])
-        self.calc_case_entry.pack(side=tk.LEFT, padx=5)
+        tar_frame = ttk.Frame(self)
+        tar_frame.pack(pady=5)
         vals = ["all", "xy_component (AB_x, AB_y)", "norm (|AB|)"]
-        self.calc_type_combo = Combobox(cross_frame, label="Calc:", width=22, values=vals)
+        self.calc_type_combo = Combobox(tar_frame, label="Calc:", width=22, values=vals)
         self.calc_type_combo.pack_horizontal(padx=5)
         data_dir = self._find_data_dir()
         img_path = os.path.join(data_dir, "img", "vector_2.gif")
         self.img = tk.PhotoImage(file=img_path)
-        self.img_label = ttk.Label(cross_frame, image=self.img)
+        self.img_label = ttk.Label(tar_frame, image=self.img)
         self.img_label.pack(side=tk.LEFT, padx=5)
-        self.member_combo = MemberKeypointComboboxesFor2Point(cross_frame)
+        self.member_combo = MemberKeypointComboboxesFor2Point(tar_frame)
         self.member_combo.pack(side=tk.LEFT, padx=5)
+        add_button = ttk.Button(tar_frame, text="Add", command=self.add_row)
+        add_button.pack(side=tk.LEFT, padx=5)
+        delete_btn = ttk.Button(tar_frame, text="Delete Selected", command=self.delete_selected)
+        delete_btn.pack(side=tk.LEFT, padx=5)
 
         setting_frame = ttk.Frame(self)
         setting_frame.pack(pady=5)
 
+        self.calc_case_entry = CalcCaseEntry(setting_frame, temp.data["calc_case"])
+        self.calc_case_entry.pack(side=tk.LEFT, padx=5)
+
         self.thinning_entry = IntEntry(setting_frame, label="Thinning:", default=temp.data["thinning"])
         self.thinning_entry.pack_horizontal(padx=5)
 
-        draw_btn = ttk.Button(setting_frame, text="Add and Draw", command=self.manual_draw)
+        draw_btn = ttk.Button(setting_frame, text="Draw", command=self.manual_draw)
         draw_btn.pack(side=tk.LEFT)
-
         clear_btn = ttk.Button(setting_frame, text="Clear", command=self.clear)
         clear_btn.pack(side=tk.LEFT, padx=(5, 0))
-
         export_btn = ttk.Button(setting_frame, text="Export", command=self.export)
         export_btn.pack(side=tk.LEFT, padx=(5, 50))
-
         repeat_btn = ttk.Button(setting_frame, text="Repeat Draw", command=self.repeat_draw)
         repeat_btn.pack(side=tk.LEFT)
+
+        tree_frame = ttk.Frame(self)
+        tree_frame.pack(pady=5)
+        cols = ("calc", "member", "A", "B")
+        self.tree = ttk.Treeview(tree_frame, columns=cols, show="headings", selectmode="browse")
+        for col in cols:
+            self.tree.heading(col, text=col)
+        self.tree.column("calc", width=300)
+        self.tree.column("member", width=200)
+        self.tree.column("A", width=50)
+        self.tree.column("B", width=50)
+        self.tree.pack()
+        self.tree.bind("<<TreeviewSelect>>", self.select_tree_row)
 
         plot_frame = ttk.Frame(self)
         plot_frame.pack(pady=5)
@@ -70,33 +85,63 @@ class App(ttk.Frame):
         self.src_attrs = self.src_df.attrs
         self.time_min, self.time_max = args["time_span_msec"]
 
+        # timestampの範囲を抽出
+        self.tar_df = keypoints_proc.filter_by_timerange(self.src_df, self.time_min, self.time_max)
+        self.tar_df = self.tar_df[~self.tar_df.index.duplicated(keep="last")]
+
         # UIの更新
         self.member_combo.set_df(self.src_df)
         self.lineplot.set_vcap(self.cap)
         self.clear()
 
-    def _combo_to_calc_code(self):
+    def _combo_to_calc_code(self, calc):
         code = ""
-        if self.calc_type_combo.get() == "xy_component (AB_x, AB_y)":
+        if calc == "xy_component (AB_x, AB_y)":
             code = "component"
-        elif self.calc_type_combo.get() == "norm (|AB|)":
+        elif calc == "norm (|AB|)":
             code = "norm"
-        elif self.calc_type_combo.get() == "all":
+        elif calc == "all":
             code = "all"
         return code
 
+    def add_row(self):
+        calc = self.calc_type_combo.get()
+        member, point_a, point_b = self.member_combo.get_selected()
+        tar_list = [k for k in self.tree.get_children("")]
+        for i, tar in enumerate(tar_list):
+            tree_calc, tree_member, tree_point_a, tree_point_b = self.tree.item(tar, "values")
+            # skip if exactly same row
+            if tree_calc == calc and tree_member == member and tree_point_a == point_a and tree_point_b == point_b:
+                return
+
+        self.tree.insert("", "end", values=(calc, member, point_a, point_b))
+
+    def delete_selected(self):
+        if len(self.tree.selection()) == 0:
+            return
+        selected = self.tree.selection()[0]
+        self.tree.delete(selected)
+
+    def select_tree_row(self, event):
+        """Handle the selection of a row in the tree."""
+        if len(self.tree.selection()) == 0:
+            return
+        selected = self.tree.selection()[0]
+        calc, member, point_a, point_b = self.tree.item(selected, "values")
+        self.calc_type_combo.update(calc)
+        self.member_combo.update(member)
+        self.point_a.update(point_a)
+        self.point_b.update(point_b)
+
     def manual_draw(self):
         """Call _draw based on the selected member and calculation code."""
-        current_member, kp0, kp1 = self.member_combo.get_selected()
-        code = self._combo_to_calc_code()
-        self._draw(current_member, code, kp0, kp1)
+        calcs = [self.tree.item(row, "values") for row in self.tree.get_children("")]
+        self._draw(calcs)
 
     def repeat_draw(self):
         """Open a file dialog to select a feature file.
         Extract the column names from the selected file.
-        Call _draw based on the selected member and extracted calculation codes.
         """
-        current_member, _, _ = self.member_combo.get_selected()
         calc_case = self.calc_case_entry.get_calc_case()
         pl = file_inout.PickleLoader(self.calc_dir, pkl_type="feature")
         pl.join_calc_case(calc_case)
@@ -104,86 +149,67 @@ class App(ttk.Frame):
         if is_file_selected is False:
             return
         in_trk_df = pl.load_pkl()
+        proc_history = in_trk_df.attrs["proc_history"]
+        for history in proc_history:
+            if isinstance(history, dict) and history["proc"] == "2p_vector":
+                src_cols = history["source_cols"]
+                break
+        for col in src_cols:
+            self.tree.insert("", "end", values=col)
+        self._draw(src_cols)
 
-        pattern = r"(\w+)\((\w+)-(\w+)\)"
-        cols = in_trk_df.columns.tolist()
-
-        # suffix '_x'と'_y'があるときは'_y'を削除、片方しかないときは削除しない
-        contains_x = any(map(lambda x: x.endswith("_x"), cols))
-        contains_y = any(map(lambda x: x.endswith("_y"), cols))
-        if contains_x and contains_y:
-            cols = [item for item in cols if not item.endswith("_y")]
-
-        for col in cols:
-            if col == "timestamp":
-                continue
-            match = re.match(pattern, col)
-            if not match:
-                continue
-            calc_code = match.group(1)
-            kp_a = match.group(2)
-            kp_b = match.group(3)
-            self._draw(current_member, calc_code, kp_a, kp_b)
-
-    def _draw(self, current_member, calc_code, kp0, kp1):
-        # timestampの範囲を抽出
-        tar_df = keypoints_proc.filter_by_timerange(self.src_df, self.time_min, self.time_max)
-        # 重複インデックス削除
-        tar_df = tar_df[~tar_df.index.duplicated(keep="last")]
-        # memberとkeypointのインデックス値を文字列に変換
-        idx = tar_df.index
-        tar_df.index = tar_df.index.set_levels([idx.levels[0], idx.levels[1].astype(str), idx.levels[2].astype(str)])
-        # current_memberのみ抽出
-        tar_df = tar_df.loc[pd.IndexSlice[:, current_member, :], :]
-
-        if calc_code == "component":
-            prod_df = keypoints_proc.calc_xy_component(tar_df, kp0, kp1)
-        elif calc_code == "norm":
-            prod_df = keypoints_proc.calc_norm(tar_df, kp0, kp1)
-        elif calc_code == "all":
-            xy_df = keypoints_proc.calc_xy_component(tar_df, kp0, kp1)
-            norm_df = keypoints_proc.calc_norm(tar_df, kp0, kp1)
-            prod_df = pd.concat([xy_df, norm_df], axis=1)
-
-        col_names = prod_df.columns
-
-        self.timestamp_df = tar_df.loc[:, "timestamp"].droplevel(2).to_frame()
-        plot_df = pd.concat([prod_df, self.timestamp_df], axis=1)
-
-        for col_name in col_names:
-            if col_name not in self.calc_df.columns:
-                add_df = plot_df.loc[:, col_name].to_frame()
-                self.calc_df = pd.concat([self.calc_df, add_df], axis=1)
+    def _draw(self, rows):
+        self.lineplot.set_trk_df(self.src_df)
+        self.source_cols = rows
 
         # thinningの値だけframeを間引く
         thinning = self.thinning_entry.get()
         self.thinning_entry.save_to_temp("thinning")
-        plot_df = keypoints_proc.thinning(plot_df, thinning)
+        thinned_df = keypoints_proc.thinning(self.tar_df, thinning)
+        idx = thinned_df.index
+        thinned_df.index = thinned_df.index.set_levels([idx.levels[0], idx.levels[1].astype(str), idx.levels[2].astype(str)])
 
-        self.lineplot.set_trk_df(self.src_df)
-        self.lineplot.set_plot(plot_df, current_member, col_names)
+        for calc, member, point_a, point_b in rows:
+            code = self._combo_to_calc_code(calc)
+            member_df = thinned_df.loc[pd.IndexSlice[:, member], :]
+            if code == "component":
+                plot_df = keypoints_proc.calc_xy_component(member_df, point_a, point_b)
+            elif code == "norm":
+                plot_df = keypoints_proc.calc_norm(member_df, point_a, point_b)
+            elif code == "all":
+                xy_df = keypoints_proc.calc_xy_component(member_df, point_a, point_b)
+                norm_df = keypoints_proc.calc_norm(member_df, point_a, point_b)
+                plot_df = pd.concat([xy_df, norm_df], axis=1)
+            col_names = plot_df.columns.tolist()
+            self.feat_df = pd.concat([self.feat_df, plot_df], axis=1)
+            plot_df["timestamp"] = thinned_df.loc[pd.IndexSlice[:, :, point_a], :].droplevel(2)["timestamp"]
+            self.lineplot.set_plot(plot_df, member, col_names)
+
         self.lineplot.draw()
+        print("done")
 
     def export(self):
         """Export the calculated data to a file."""
-        if len(self.calc_df) == 0:
+        if len(self.feat_df) == 0:
             print("No data to export.")
             return
+        self.feat_df = self.feat_df.sort_index()
         file_name = os.path.splitext(self.src_attrs["video_name"])[0]
-        timestamp_df = self.timestamp_df
+        first_keypoint_id = self.tar_df.index.get_level_values(2).values
+        timestamp_df = self.tar_df.loc[pd.IndexSlice[:, :, first_keypoint_id], :].droplevel(2)["timestamp"]
         timestamp_df = timestamp_df[~timestamp_df.index.duplicated(keep="last")]
-        self.calc_df = self.calc_df[~self.calc_df.index.duplicated(keep="last")]
-        export_df = pd.concat([self.calc_df, timestamp_df], axis=1)
+        self.feat_df = self.feat_df[~self.feat_df.index.duplicated(keep="last")]
+        export_df = pd.concat([self.feat_df, timestamp_df], axis=1)
         export_df.attrs = self.src_attrs
         calc_case = self.calc_case_entry.get_calc_case()
         dst_path = os.path.join(self.calc_dir, calc_case, file_name + "_2p.feat.pkl")
-        history_dict = {"proc": "2p_vector", "source_cols": []}
+        history_dict = {"proc": "2p_vector", "source_cols": self.source_cols}
         file_inout.save_pkl(dst_path, export_df, proc_history=history_dict)
 
     def clear(self):
-        """Clear the lineplot and reset the calc_df."""
+        """Clear the lineplot and reset the feat_df."""
         self.lineplot.clear()
-        self.calc_df = pd.DataFrame()
+        self.feat_df = pd.DataFrame()
 
     def _find_data_dir(self):
         if getattr(sys, "frozen", False):
