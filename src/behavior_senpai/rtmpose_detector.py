feef6eea31efd4e0b1c3d8a396a9398b7700b918
nishimura5 <super.nishimura@gmail.com>
2025-11-14 17:54:56 +0900

    Add configuration files for QDTrack model and update RTMPoseDetector for tracking

--- src/behavior_senpai/rtmpose_detector.py
+++ src/behavior_senpai/rtmpose_detector.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import pandas as pd
-from mmdet.apis import inference_detector, init_detector
+from mmdet.apis import inference_detector, inference_mot, init_detector, init_track_model
 from mmengine.registry import init_default_scope
 from mmpose.apis import inference_topdown, init_model
 from mmpose.evaluation.functional import nms
@@ -25,24 +25,38 @@ class RTMPoseDetector:
             config = "./mm_config/rtmw-x_8xb320-270e_cocktail14-384x288.py"
             checkpoint = "https://download.openmmlab.com/mmpose/v1/projects/rtmw/rtmw-x_simcc-cocktail14_pt-ucoco_270e-384x288-f840f204_20231122.pth"
             self.number_of_keypoints = 133
+
+        # detection
         det_config = "./mm_config/rtmdet_m_640-8xb32_coco-person.py"
         det_checkpoint = "https://download.openmmlab.com/mmpose/v1/projects/rtmpose/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth"
         self.det_model = init_detector(det_config, det_checkpoint, device="cuda:0")
+
+        # tracking
+        track_config = "./mm_config/qdtrack_faster-rcnn_r50_fpn_8xb2-4e_mot17halftrain_test-mot17halfval.py"
+        track_checkpoint = (
+            "https://download.openmmlab.com/mmtracking/mot/qdtrack/mot_dataset/qdtrack_faster-rcnn_r50_fpn_4e_mot17_20220315_145635-76f295ef.pth"
+        )
+        self.track_model = init_track_model(track_config, track_checkpoint, device="cuda:0")
+
         self.pose_model = init_model(config, checkpoint, device="cuda:0")
         self.visualizer = VISUALIZERS.build(self.pose_model.cfg.visualizer)
         self.visualizer.set_dataset_meta(self.pose_model.dataset_meta)
 
-        self.det_score_threshold = 0.3
+        self.det_score_threshold = 0.1
         self.pose_score_threshold = 0.3
         self.retain_threshold = 0.3
         self.det_cat_id = 0
         self.show = show
+
+        self.frame_id = 0
 
     def set_cap(self, cap):
         self.cap = cap
         self.total_frame_num = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
         self.frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        self.frame_id = 0
 
     def detect(self, roi=False):
         # データの初期化
@@ -57,17 +71,32 @@ class RTMPoseDetector:
                 continue
             rgb_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            scope = self.det_model.cfg.get("default_scope", "mmdet")
+            scope = self.track_model.cfg.get("default_scope", "mmdet")
             if scope is not None:
                 init_default_scope(scope)
             # bbox検出
-            det_result = inference_detector(self.det_model, rgb_img)
-            pred_instance = det_result.pred_instances.cpu().numpy()
-            bboxes = np.concatenate((pred_instance.bboxes, pred_instance.scores[:, None]), axis=1)
-            bboxes = bboxes[np.logical_and(pred_instance.labels == self.det_cat_id, pred_instance.scores > self.det_score_threshold)]
-            bboxes = bboxes[nms(bboxes, self.retain_threshold), :4]
-            # x座標でソート
-            bboxes = bboxes[bboxes[:, 0].argsort()]
+            track_result = inference_mot(self.track_model, rgb_img, frame_id=self.frame_id, video_len=self.total_frame_num)
+            self.frame_id += 1
+            if isinstance(track_result, list):
+                track_data_sample = track_result[0]
+            else:
+                track_data_sample = track_result
+            det_sample = track_data_sample.video_data_samples[0]
+            track_instances = det_sample.pred_track_instances
+            bboxes = track_instances.bboxes.cpu().numpy()
+            scores = track_instances.scores.cpu().numpy()
+            labels = track_instances.labels.cpu().numpy()
+            track_ids = track_instances.instances_id.cpu().numpy()
+            # 人クラスだけ＆スコアしきい値で絞る
+            keep = (labels == self.det_cat_id) & (scores > self.det_score_threshold)
+            bboxes = bboxes[keep]
+            track_ids = track_ids[keep]
+            scores = scores[keep]
+
+            # 何もなければ次のフレームへ
+            if len(bboxes) == 0:
+                continue
+
             # keypoint検出
             results = inference_topdown(self.pose_model, rgb_img, bboxes)
             data_samples = merge_data_samples(results)
